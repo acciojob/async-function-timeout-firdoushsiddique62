82bfa3bcf8cc2b55de9fd6c9dce99078cbdfe5f1
FIRDOUSH ALI SIDDIQUE <115089495+firdoushsiddique62@users.noreply.github.com>
2023-08-31 19:55:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+//your JS code here. If required.
+
+let text = document.getElementById("text");
+let delay =document.getElementById("delay");
+let output =  document.getElementById("output");
+let	btn  = document.getElementById("btn");
+ btn.addEventListener("click" , Resolve);
+
+
+async function Resolve(){
+	
+	let ans = await getMessage();
+	output.innerHTML = ans;
+}
+
+function getMessage(){
+	let prom = new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve(text.value);
+		},Number(delay.value)*1000);
+	})
+
+  let x = prom.then((data)=>{
+		return data;
+	});
+	
+
+	return x;
+}
